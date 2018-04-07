215e9db43f3c23ac8817fba55170c82338fc4f3a
Kohei Asai <me@axross.io>
2018-04-07 19:43:58 +0900

    Add theme

---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+// type Theme = {
+//   spacing: {
+//     small: number;
+//     regular: number;
+//     large: number;
+//     huge: number;
+//     enormous: number;
+//   };
+// };
+
+const theme = {
+  spacing: {
+    small: 8,
+    regular: 16,
+    large: 24,
+    huge: 40,
+    enormous: 80,
+  },
+};
+
+export default theme;
