4d476358e17504901773d8b569d4170885101168
Kohei Asai <me@axross.io>
2018-04-12 10:51:10 +0900

    Fix test to pass

--- src/view/page/IndexPage.test.tsx
+++ src/view/page/IndexPage.test.tsx
@@ -12,8 +12,8 @@ describe('<IndexPage>', () => {
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
     const workExperiences = [
       new WorkExperience({
-        startedAt: DateTime.fromMillis(0, { locale: 'utc' }),
-        endedAt: DateTime.fromMillis(0, { locale: 'utc' }),
+        startedAt: DateTime.fromMillis(0, { zone: 'utc' }),
+        endedAt: DateTime.fromMillis(0, { zone: 'utc' }),
         employeeName: 'Eighty Three, Inc.',
         employeeThumbnailImageUrl: 'https://eighty-three.co.jp/',
         summary: 'Yeah',
